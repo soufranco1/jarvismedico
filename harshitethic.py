cce1759578c358f847e20f7ff54c825c57831840
soufranco1 <123389784+soufranco1@users.noreply.github.com>
2023-01-23 12:23:33 -0400

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -35,8 +35,8 @@ running = False
 cache = None
 qcache = None
 chat_log = None
-botname = 'Jarvismedico Franco'
-username = 'jarvismedico_bot'
+botname = 'Harshit ethic'
+username = 'harshitethic_bot'
 # Max chat log length (A token is about 4 letters and max tokens is 2048)
 max = int(3000)
 
